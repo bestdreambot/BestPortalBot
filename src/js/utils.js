30eb70c4c5da6654352a5370c1a6cd6a305da837
bestdreambot <212590801+bestdreambot@users.noreply.github.com>
2025-08-25 02:54:57 +0300

    Автообновление из update.zip @ Mon Aug 25 02:54:44 2025

--- src/js/utils.js
+++ src/js/utils.js
@@ -1,10 +1,1 @@
-
-const Utils={
-  preload(){
-    ["/static/img/BestPortal.jpg","/static/img/Noira.jpg","/static/img/Zaryum.jpg","/static/img/lunora.png"]
-      .forEach(s=>{const i=new Image();i.src=s});
-    ["/static/anim/bestportal.mp4","/static/anim/lunora.mp4"]
-      .forEach(s=>{const v=document.createElement("video");v.src=s;v.preload="auto"});
-  },
-  debug(m){const el=document.getElementById('debug'); if(el) el.textContent=m||'';}
-};
+export const dbg=(m)=>{const el=document.getElementById('debug');if(el) el.textContent=m||'';};
