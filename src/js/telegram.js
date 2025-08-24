37b58c4d773e9f10028ebe18616506ba2c075303
bestdreambot <212590801+bestdreambot@users.noreply.github.com>
2025-08-25 02:54:54 +0300

    Автообновление из update.zip @ Mon Aug 25 02:54:44 2025

--- src/js/telegram.js
+++ src/js/telegram.js
@@ -1,13 +1,1 @@
-
-const TelegramAPI={
-  init(){
-    const tg=window.Telegram?.WebApp; if(!tg) return null;
-    try{
-      tg.ready(); tg.expand();
-      tg.setBackgroundColor('#000000'); tg.setHeaderColor('#000000');
-      [0,150,600].forEach(d=>setTimeout(()=>tg.expand(),d));
-      window.addEventListener('orientationchange',()=>setTimeout(()=>tg.expand(),300));
-      return tg;
-    }catch(e){console.error('TG init error',e);return null;}
-  }
-};
+export function initTelegram(){try{const tg=window.Telegram?.WebApp;if(tg){tg.ready();tg.expand();tg.setBackgroundColor('#000');tg.setHeaderColor('#000');}}catch(e){}}
